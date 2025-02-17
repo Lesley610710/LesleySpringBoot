6ef5b334138d022afc7fe0b7c3fcbf697fed2199
Lesley610710 <yilingl6@student.unimelb.edu.au>
2025-02-18 00:46:02 +1100

    GET request from frontend to backend

--- src/js/src/App.js
+++ src/js/src/App.js
@@ -1,7 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
-
+import { getAllStudents } from './client';
 function App() {
+  getAllStudents().then(res => res.json().then(students => {
+    console.log(students);
+  }));
   return (
     <div className="App">
       <header className="App-header">
