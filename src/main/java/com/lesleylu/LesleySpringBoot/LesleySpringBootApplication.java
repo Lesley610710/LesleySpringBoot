213dd08882d6e12a3e6c9c135263be154a366d52
Lesley610710 <yilingl6@student.unimelb.edu.au>
2025-02-17 22:16:40 +1100

    project initialization and model define

---
+++ src/main/java/com/lesleylu/LesleySpringBoot/LesleySpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.lesleylu.LesleySpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LesleySpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LesleySpringBootApplication.class, args);
+	}
+
+}
